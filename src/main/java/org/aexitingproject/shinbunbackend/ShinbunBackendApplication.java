346b5ea7dc448e2f708d2d7a3efdc28bfb944b00
aexiting <adamttway@gmail.com>
2025-05-06 16:42:41 -0400

    initial commit for project

---
+++ src/main/java/org/aexitingproject/shinbunbackend/ShinbunBackendApplication.java
@@ -0,0 +1,13 @@
+package org.aexitingproject.shinbunbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinbunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinbunBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aexitingproject/shinbunbackend/ShinbunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aexitingproject.shinbunbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinbunBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
