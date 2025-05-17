1b84bf5c5c207ef15aeaf2bdd1324542909bbaee
aexiting <adamttway@gmail.com>
2025-05-16 20:25:03 -0400

    Removing again

---
+++ src/main/java/org/aexitingproject/shinbunbackend/ShinbunBackendApplication.java
@@ -0,0 +1,8 @@
+package org.aexitingproject.shinbunbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinbunBackendApplication {
+}
